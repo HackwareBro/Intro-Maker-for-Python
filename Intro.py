0c755a368a6bce58b0a0bab60220455d539bab49
HackwareBro <hackwarebro@gmail.com>
2021-08-01 14:58:08 +0500

    title rotation method and testing method has been added!

--- Intro.py
+++ Intro.py
@@ -9,15 +9,21 @@ colorama.init()
 #Color changing Title
 def blinking(title, cycle=5,color_shade = [Fore.CYAN,Fore.BLUE,Fore.MAGENTA]):   
     for color in range(len(color_shade)*cycle):
-        print(Fore.RESET,'Created By:')
         print(color_shade[color%3])
         print(figlet_format(title,font='speed'))
         sleep(0.25)
         os.system('cls')
+    print(f'{Fore.RESET}{Back.RESET}') #To reset the colors
 
-# #Title rotation
-# for color in range(4*10):
-#     font = 'isometric' + str(color%4 + 1)
-#     print(figlet_format("Hackware Bro",font=font))
-#     sleep(0.25)
-#     os.system('cls')
+#Title rotation
+def rotating(title, cycle=5):
+    for color in range(4*cycle):
+        font = 'isometric' + str(color%4 + 1)
+        print(figlet_format(title,font=font))
+        sleep(0.25)
+        os.system('cls')
+
+#For testing
+if __name__ == "__main__":
+    blinking('Hackware Bro', 3)
+    rotating('HB')
